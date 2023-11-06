2845c86735e0c3cf93ce197d8c3b210c072c6bf6
Alan Rostami <alan.rostami@artfx.fr>
2023-11-06 14:15:31 +0100

    Add test SDL window

--- GameEnginePong/GameEnginePong/Main.cpp
+++ GameEnginePong/GameEnginePong/Main.cpp
@@ -3,6 +3,27 @@
 
 int main(int argc, char** argv)
 {
-	std::cout << "Game Engine" << std::endl;
-	return 0;
+    // Screen dimension constants
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 450;
+
+    // Window pointer
+    SDL_Window* window = nullptr;
+
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create window
+    window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    // Wait two seconds
+    SDL_Delay(2000);
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL
+    SDL_Quit();
+
+    return 0;
 }
